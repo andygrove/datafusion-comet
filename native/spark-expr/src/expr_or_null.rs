5c16012c7d93e01e1ab67dc940ace5f1e92f8682
Andy Grove <agrove@apache.org>
2024-07-15 18:06:59 -0600

    invert the bitmask, handle scalar expr

--- native/spark-expr/src/expr_or_null.rs
+++ native/spark-expr/src/expr_or_null.rs
@@ -15,19 +15,22 @@
 // specific language governing permissions and limitations
 // under the License.
 
-use crate::utils::down_cast_any_ref;
-use arrow::compute::nullif;
-use arrow_array::{Array, BooleanArray, RecordBatch};
-use arrow_schema::{DataType, Schema};
-use datafusion_common::Result;
-use datafusion_expr::ColumnarValue;
-use datafusion_physical_plan::{DisplayAs, DisplayFormatType, PhysicalExpr};
 use std::{
     any::Any,
     fmt::{Debug, Display, Formatter},
     hash::{Hash, Hasher},
     sync::Arc,
 };
+
+use arrow::compute::nullif;
+use arrow_array::{Array, BooleanArray, RecordBatch};
+use arrow_schema::{DataType, Schema};
+
+use datafusion_common::{DataFusionError, Result};
+use datafusion_expr::ColumnarValue;
+use datafusion_physical_plan::{DisplayAs, DisplayFormatType, PhysicalExpr};
+
+use crate::utils::down_cast_any_ref;
 
 /// Specialization of `CASE WHEN predicate THEN expr ELSE null END`
 #[derive(Debug, Hash)]
@@ -46,14 +49,22 @@ impl ExprOrNull {
 }
 
 impl Display for ExprOrNull {
-    fn fmt(&self, _f: &mut Formatter<'_>) -> std::fmt::Result {
-        todo!()
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "ExprOrNull(predicate={}, expr={})",
+            self.predicate, self.expr
+        )
     }
 }
 
 impl DisplayAs for ExprOrNull {
-    fn fmt_as(&self, _t: DisplayFormatType, _f: &mut Formatter) -> std::fmt::Result {
-        todo!()
+    fn fmt_as(&self, _: DisplayFormatType, f: &mut Formatter) -> std::fmt::Result {
+        write!(
+            f,
+            "ExprOrNull(predicate={}, expr={})",
+            self.predicate, self.expr
+        )
     }
 }
 
@@ -71,32 +82,36 @@ impl PhysicalExpr for ExprOrNull {
     }
 
     fn evaluate(&self, batch: &RecordBatch) -> Result<ColumnarValue> {
-        // evaluate predicate
         if let ColumnarValue::Array(bit_mask) = self.predicate.evaluate(batch)? {
             let bit_mask = bit_mask
                 .as_any()
                 .downcast_ref::<BooleanArray>()
                 .expect("predicate should evaluate to a boolean array");
-            if let ColumnarValue::Array(array) = self.expr.evaluate(batch)? {
-                //TODO need to invest the bitmask
-                Ok(ColumnarValue::Array(nullif(&array, bit_mask)?))
-            } else {
-                panic!()
+            // invert the bitmask
+            let bit_mask = arrow::compute::kernels::boolean::not(bit_mask)?;
+            match self.expr.evaluate(batch)? {
+                ColumnarValue::Array(array) => Ok(ColumnarValue::Array(nullif(&array, &bit_mask)?)),
+                ColumnarValue::Scalar(value) => {
+                    let array = value.to_array_of_size(batch.num_rows())?;
+                    Ok(ColumnarValue::Array(nullif(&array, &bit_mask)?))
+                }
             }
         } else {
-            panic!()
+            Err(DataFusionError::Execution(
+                "predicate did not evaluate to an array".to_string(),
+            ))
         }
     }
 
     fn children(&self) -> Vec<&Arc<dyn PhysicalExpr>> {
-        todo!()
+        vec![]
     }
 
     fn with_new_children(
         self: Arc<Self>,
         _children: Vec<Arc<dyn PhysicalExpr>>,
     ) -> Result<Arc<dyn PhysicalExpr>> {
-        todo!()
+        Ok(self)
     }
 
     fn dyn_hash(&self, state: &mut dyn Hasher) {
