b28f8340e639cd3ada96e4c735ea372997675de0
Andy Grove <agrove@apache.org>
2024-09-23 12:21:49 -0600

    improve

--- native/tools/src/main.rs
+++ native/tools/src/main.rs
@@ -38,30 +38,76 @@ fn extract_native_metrics(path: &PathBuf) -> io::Result<()> {
     let reader = io::BufReader::new(file);
 
     let mut map: HashMap<String, Stats> = HashMap::new();
-    let re = Regex::new(r"^\s*(.*Exec).*elapsed_compute=([0-9.]*)([a-zµ]*)").unwrap();
+    let re =
+        Regex::new(r"^\s*(.*Exec|SortMergeJoin).*(elapsed_compute|join_time)=([0-9.]*)([a-zµ]*)")
+            .unwrap();
 
     for line in reader.lines() {
         let line = line?;
         if let Some(captures) = re.captures(&line) {
             let operator = captures.get(1).unwrap().as_str();
-            let time = captures.get(2).unwrap().as_str().parse::<f64>().unwrap();
-            let unit = captures.get(3).unwrap().as_str();
-            let time_unit = match unit {
-                "s" => 1000000000.0,
-                "ms" => 1000000.0,
-                "µs" => 1000.0,
-                "ns" => 1.0,
-                _ => unreachable!("unknown unit: {unit}"),
-            };
-            let stats = map.entry(operator.to_string()).or_insert(Stats::default());
-            stats.count += 1;
-            stats.time_nanos += time * time_unit;
+            let time = captures.get(3).unwrap().as_str().parse::<f64>().unwrap();
+            let unit = captures.get(4).unwrap().as_str();
+            if let Some(time_unit) = parse_time_unit(&line, unit) {
+                let unique_exec_str: String = match operator {
+                    "ScanExec" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, "source=", ", metrics").unwrap_or("")
+                    ),
+                    "FilterExec" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, " ", ", metrics").unwrap_or("")
+                    ),
+                    "SortExec" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, "expr=[", "]").unwrap_or("")
+                    ),
+                    "ShuffleWriterExec" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, "partitioning=", "}]").unwrap_or("")
+                    ),
+                    "AggregateExec" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, "mode=", ", metrics").unwrap_or("")
+                    ),
+                    "HashJoinExec" | "SortMergeJoin" => format!(
+                        "{operator}: {}",
+                        extract_between(&line, "join_type=", "]").unwrap_or("")
+                    ),
+                    _ => operator.to_string(),
+                };
+                let stats = map.entry(unique_exec_str).or_insert(Stats::default());
+                stats.count += 1;
+                stats.time_nanos += time * time_unit;
+            }
         }
     }
 
     for (k, v) in &map {
-        println!("{k},{},{} ms", v.count, (v.time_nanos / 1000000.0) as i64);
+        println!("{}\t{}\t{k}", v.count, (v.time_nanos / 1000000.0) as i64);
     }
 
     Ok(())
+}
+
+fn extract_between<'a>(input: &'a str, start: &'a str, end: &'a str) -> Option<&'a str> {
+    if let (Some(start), Some(end)) = (input.find(start), input.find(end)) {
+        if start < end {
+            return Some(&input[start..=end]);
+        }
+    }
+    None
+}
+
+fn parse_time_unit(line: &str, unit: &str) -> Option<f64> {
+    match unit {
+        "s" => Some(1000000000.0),
+        "ms" => Some(1000000.0),
+        "µs" => Some(1000.0),
+        "ns" => Some(1.0),
+        _ => {
+            eprintln!("unknown unit '{unit}' in {line}");
+            None
+        }
+    }
 }
